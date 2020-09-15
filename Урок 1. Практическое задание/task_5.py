fa3f78752c6f50a8fbcf4947ad1967520d9c9d9e
Semyon <schweppes2205@gmail.com>
2020-09-15 15:55:49 +0300

    tasks 5, 7. 6th task is not added

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -18,3 +18,32 @@
 
 После реализации структуры, проверьте ее работу на различных сценариях
 """
+
+
+class StackClass:
+    def __init__(self):
+        self.stack_obj = []
+        self.plate_threshold = 10
+        self.active_stack_number = 0
+
+    def push_in(self):
+        if len(self.stack_obj) == 0:
+            self.stack_obj.append(["plate"])
+            print("It's a very first plate here")
+        elif len(self.stack_obj[self.active_stack_number]) < self.plate_threshold:
+            self.stack_obj[self.active_stack_number].append("plate")
+            print(f"Adding a new plate. Now you have: Plate stack number: {self.active_stack_number + 1}."
+                  f" Plate number in the last stack: {len(self.stack_obj[self.active_stack_number])}")
+        else:
+            self.active_stack_number += 1
+            self.stack_obj.append(["plate"])
+            print(f"We started a new plate stack with number {self.active_stack_number + 1} with one plate.")
+
+    def stack_size(self):
+        return self.active_stack_number
+
+
+if __name__ == '__main__':
+    my_stack = StackClass()
+    for i in range(25):
+        my_stack.push_in()
