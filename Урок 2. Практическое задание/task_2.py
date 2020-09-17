8ccd3c800da856d6619e495113b7fc75066ab0af
Semyon <schweppes2205@gmail.com>
2020-09-17 15:21:29 +0300

    lecture02, task02

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -16,3 +16,53 @@
 Введите число: 123
 Количество четных и нечетных цифр в числе равно: (1, 2)
 """
+
+
+def even_odd_cycle(n):
+    e = 0
+    o = 0
+    while n / 10 >= 0.1:
+        if n % 10 % 2 == 0:
+            e = e + 1
+        else:
+            o = o + 1
+        n = n // 10
+    return f"even:{e}, odd:{o}"
+
+
+def even_odd_lst(n):
+    if n / 10 <= 0.1:
+        if n % 2 == 0:
+            return "e"
+        else:
+            return "o"
+    else:
+        if n%10%2 == 0:
+            return "e" + even_odd_lst(n//10)
+        else:
+            return "o" + even_odd_lst(n//10)
+
+
+def even_odd_dict(n):
+    if n / 10 <= 0.1:
+        if n % 2 == 0:
+            return {"e": 1, "o": 0}
+        else:
+            return {"e": 0, "o": 1}
+    else:
+        if n % 10 % 2 == 0:
+            return {"e":(even_odd_dict(n//10))["e"]+1, "o":(even_odd_dict(n//10))["o"]}
+        else:
+            return {"e":(even_odd_dict(n//10))["e"], "o":(even_odd_dict(n//10))["o"]+1}
+
+
+print(f"cycle solution. {even_odd_cycle(11234)}")
+even_count = 0
+odd_count = 0
+for i in even_odd_lst(11234):
+    if i == "e":
+        even_count = even_count + 1
+    else:
+        odd_count = odd_count + 1
+print(f"first recursion solution. even:{even_count}, odd:{odd_count}")
+print(f"second recursion solution. even:{even_odd_dict(11234)['e']}, odd:{even_odd_dict(11234)['o']}")
