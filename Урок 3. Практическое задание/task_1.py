2bc99c0ccc0f91e8ced7075e4bd6a51b75fd593d
Semyon <schweppes2205@gmail.com>
2020-09-20 21:39:12 +0300

    lecture03, task01

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -10,3 +10,37 @@
 то реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к двум своим функциям.
 """
+from random import randint
+import time, datetime
+
+
+def time_deco(func):
+    def wrapper(n):
+        print(f"Function name: {func.__name__}. Start time: {datetime.datetime.now().time()}.")
+        start_time = time.time()
+        func(n)
+        stop_time = time.time()
+        print(f"Start time: {datetime.datetime.now().time()}.")
+        print(f"Execution time: {'{:.2f}'.format(stop_time - start_time)} sec.")
+    return wrapper
+
+
+@time_deco
+def fill_lst(n):
+    new_lst = []
+    for i in range(n):
+        new_lst.append(randint(1, 10))
+    return
+
+
+@time_deco
+def fill_dict(n):
+    new_dict = {}
+    for i in range(n):
+        new_dict[n] = randint(1, 10)
+    return
+
+
+items_count = 1000000
+fill_lst(items_count)
+fill_dict(items_count)
