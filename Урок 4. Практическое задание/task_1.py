313718eb1aeea98de6e533ec685900ae999702fe
Semyon <schweppes2205@gmail.com>
2020-09-25 09:21:30 +0300

    lecture04, task01 corrections

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,15 +13,7 @@
 Задача - заполнить массив четными числами, граница которого - конец исходного массива.
 Можно воспользоваться генератором списка, указав в качестве итератора range с шагом 2, конец которого равен длинне
 входного массива.
-
-По какой-то причине, если в func_2 передавать просто число = размер исходного массива, замер дает худший результат.
-По непонятной мне причине вывод операции вычисления длинны масива из функции во вне негативно влияет на
-производительность. Пример func_3.
-Полагаю, что вычисление длины массива, натравленного на генератор производить вычисление не один раз, а при каждой
-итерации генератора, но подтверждения этому у меня нет. Уменьшение времени выполнения можно посмотреть на примере
-замера с именем func_3_timer_second_attempt, где операция вычисления длины массива отделена от генератора исходного
-массива.
-Спасибо за интересную задачу =)
+Можно незначительно улучшить результат, вынеся операцию вычисления длинны исходного массива за функцию - func_3
 """
 
 import timeit
@@ -45,23 +37,14 @@ def func_3(arr_len):
     return new_arr
 
 
-func_1_timer = timeit.Timer("""
-test_arr = [i for i in range(10000)]
-func_1(test_arr)""", "from __main__ import func_1")
-func_2_timer = timeit.Timer("""
-test_arr = [i for i in range(10000)]
-func_2(test_arr)""", "from __main__ import func_2")
-func_3_timer = timeit.Timer("""
-test_arr_len = len([i for i in range(10000)])
-func_3(test_arr_len)""", "from __main__ import func_3")
-func_3_timer_second_attempt = timeit.Timer("""
 test_arr = [i for i in range(10000)]
 test_arr_len = len(test_arr)
-func_3(test_arr_len)""", "from __main__ import func_3")
+func_1_timer = timeit.Timer("func_1(test_arr)", "from __main__ import func_1, test_arr")
+func_2_timer = timeit.Timer("func_2(test_arr)", "from __main__ import func_2, test_arr")
+func_3_timer = timeit.Timer("func_3(test_arr_len)", "from __main__ import func_3,test_arr_len")
 print(func_1_timer.timeit(number=100))
 print(func_2_timer.timeit(number=100))
 print(func_3_timer.timeit(number=100))
-print(func_3_timer_second_attempt.timeit(number=100))
 test_arr = [i for i in range(10)]
 print(func_1(test_arr))
 print(func_2(test_arr))
