b5a3bb52c415ff54d6b6bb42a2bc7455a76c2377
Semyon <schweppes2205@gmail.com>
2020-09-29 11:56:41 +0300

    lecture04, task05

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,49 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+
+from collections import namedtuple, defaultdict
+
+company_data_pattern = namedtuple("Company",
+                                  "name "
+                                  "first_quarter_income "
+                                  "second_quarter_income "
+                                  "third_quarter_income "
+                                  "fourth_quarter_income "
+                                  "avg_income")
+
+user_input_company_count = int(input("Please enter the amount of companies for input: "))
+company_data = defaultdict(str)
+for i in range(user_input_company_count):
+    user_input_comp_name = input(f"#{i}. Please enter company name: ")
+    user_input_comp_income = input(f"Please enter 4 digits for {user_input_comp_name} "
+                                   f"as according income for 1-4 quarters: ").split(" ")
+    income_int = list(map(int, user_input_comp_income))
+    avg_income_tmp = sum(income_int)/len(income_int)
+    company_data[i] = company_data_pattern(
+        name=user_input_comp_name,
+        first_quarter_income=user_input_comp_income[0],
+        second_quarter_income=user_input_comp_income[1],
+        third_quarter_income=user_input_comp_income[2],
+        fourth_quarter_income=user_input_comp_income[3],
+        avg_income=str(avg_income_tmp)
+    )
+avg_income_all_lst = []
+for i in company_data.keys():
+    avg_income_all_lst.append(int(company_data[i].first_quarter_income))
+    avg_income_all_lst.append(int(company_data[i].second_quarter_income))
+    avg_income_all_lst.append(int(company_data[i].third_quarter_income))
+    avg_income_all_lst.append(int(company_data[i].fourth_quarter_income))
+avg_income_all = sum(avg_income_all_lst)/len(avg_income_all_lst)
+above_avg, below_avg = [], []
+for i in company_data.keys():
+    if float(company_data[i].avg_income) >= avg_income_all:
+        above_avg.append(company_data[i].name)
+    else:
+        below_avg.append(company_data[i].name)
+print("raw data:")
+for i in company_data.keys():
+    print(company_data[i])
+print(f"overall avg: {avg_income_all}")
+print(f"above avg: {above_avg}")
+print(f"below avg: {below_avg}")
