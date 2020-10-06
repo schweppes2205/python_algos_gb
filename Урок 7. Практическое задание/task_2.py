8140e7d67dd6e6d107564c824e8ae1b1e7696217
Semyon <schweppes2205@gmail.com>
2020-10-06 13:10:20 +0300

    lecture07, task02

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,28 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+from random import randint
+
+
+def merge_sort(lst):
+    if len(lst) == 1:
+        return lst
+    else:
+        middle = len(lst) // 2
+        left_part = merge_sort(lst[:middle])
+        right_part = merge_sort(lst[middle:])
+        res = []
+        while len(left_part) >= 1 and len(right_part) >= 1:
+            if left_part[0] < right_part[0]:
+                res.append(left_part[0])
+                left_part.pop(0)
+            else:
+                res.append(right_part[0])
+                right_part.pop(0)
+        res += left_part if len(left_part) >= 1 else right_part
+    return res
+
+
+a = [randint(0, 1000000)/100000 for i in range(10)]
+print(f"Original list:\t{a}")
+print(f"sorted list:\t{merge_sort(a)}")
